94b00238799028452ea71d8c6efc91c73365ebaa
heyongqi10 <heyongqi10@jd.com>
2025-08-21 19:57:24 +0800

    feat: 新增打包nutui-react脚本

---
+++ build_react.sh
@@ -0,0 +1,3 @@
+cd nutui-react-fork
+pnpm run build
+cp -r /Users/heyongqi10/jd-workspace/nutui-all-in-one/nutui-react-fork/release/h5/dist /Users/heyongqi10/jd-workspace/nutui-all-in-one/nutui-react-playground/node_modules/@nutui/nutui-react
