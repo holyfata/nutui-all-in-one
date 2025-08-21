1e55c2eb6b184f95732710fdf5c5e64497fff2ba
heyongqi10 <heyongqi10@jd.com>
2025-08-21 17:44:43 +0800

    feat: 完善小服务器

--- nutui-react-mini-server/index.ts
+++ nutui-react-mini-server/index.ts
@@ -1,4 +1,4 @@
-import cors from "cors"
+import cors from "cors";
 import fs from "node:fs";
 import multer from "multer";
 import path from "node:path";
@@ -6,31 +6,45 @@ import express, { type Request, type Response } from "express";
 
 const app = express();
 
-app.use(cors())
-app.use(multer({ dest: './files/' }).any())
-app.use(express.static('files'))
+// 启用跨域资源共享
+app.use(cors());
 
+// 配置 multer 中间件，上传文件保存到 ./files/ 目录
+app.use(multer({ dest: './files/' }).any());
+
+// 提供静态文件服务，访问 files 目录下的文件
+app.use(express.static('files'));
+
+// 根路由，返回 index.html 文件
 app.get("/", (_req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, './index.html'))
-})
+    res.sendFile(path.join(__dirname, './index.html'));
+});
 
+// 文件上传接口
 app.post("/upload", (req: Request, res: Response) => {
     const files = req.files as Express.Multer.File[];
     if (!files || files.length === 0) {
-        res.status(4000).send("No File Uploaded")
+        // 未上传文件，返回错误
+        res.status(400).json({ error: "No File Uploaded" });
     } else {
-        const file = files[0]
-        const filename = file?.path + path.parse(file?.originalname!).ext
-        fs.rename(file?.path!, filename, (err) => {
+        const file = files[0]!;
+        // 生成带原始扩展名的新文件名
+        const filename = file.path + path.parse(file.originalname).ext;
+        // 重命名文件，保留原始扩展名
+        fs.rename(file.path, filename, (err) => {
             if (err) {
-                res.send(res)
+                // 文件保存失败，返回错误
+                res.status(500).json({ error: "File Save Error" });
             } else {
-                res.send(`http://127.0.0.1:3000/${filename.replace('tmp\\', '')}`);
+                // 返回文件可访问的 URL
+                const filePath = `http://127.0.0.1:3000/${path.basename(filename)}`;
+                res.json({ filePath });
             }
-        })
+        });
     }
-})
+});
 
+// 启动服务器，监听 3000 端口
 app.listen(3000, () => {
-    console.log('NutUI React Mini Server is Running on http://127.0.0.1:3000...')
-})
+    console.log('NutUI React Mini Server is Running on http://127.0.0.1:3000...');
+});
