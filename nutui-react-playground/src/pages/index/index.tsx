f5e290bced83902492401519bf081cea01268dd5
heyongqi10 <heyongqi10@jd.com>
2025-08-21 19:49:03 +0800

    feat: 复现上传问题

--- nutui-react-playground/src/pages/index/index.tsx
+++ nutui-react-playground/src/pages/index/index.tsx
@@ -1,6 +1,6 @@
 import { View } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
-import { NoticeBar } from '@nutui/nutui-react'
+import { NoticeBar, Uploader, UploaderFileItem } from '@nutui/nutui-react'
 import './index.scss'
 
 export default function Index () {
@@ -10,9 +10,32 @@ export default function Index () {
 
   const text = 'Welcome to NutUI React Playground.'
 
+  const upload = async (file: File) => {
+    console.log("@hf===>", 'file: ', file)
+    const formData = new FormData();
+    formData.append('file', file)
+    const response = await fetch('http://127.0.0.1:3000/upload', {
+        method: 'POST',
+        body: formData
+    });
+    const result = await response.json();
+    const { filePath } = result
+    return { url: filePath }
+  }
+
+  const onFileChange = (files: UploaderFileItem[]) => {
+    console.error('@hf===>onFileChange', files)
+  }
+
   return (
     <View className='index'>
       <NoticeBar content={text} />
+      <Uploader 
+        upload={(file: File) => upload(file)}
+        previewType='list'
+        uploadLabel="上传文件"
+        onChange={onFileChange}
+      />
     </View>
   )
 }
